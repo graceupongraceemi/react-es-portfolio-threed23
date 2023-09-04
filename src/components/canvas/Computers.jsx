67a51fee4a0eb2e57dc82cb24b10cab02271192c
Emiko Esther S <graceupongraceemi@gmail.com>
2023-09-03 21:04:58 -0700

    useEffect and mesh in Computers.jsx modified for mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,19 +21,21 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 
-const ComputersCanvas = () => {
+const ComputersCanvas = ({ isMobile }) => {
   cost[(isMobile, setIsMobile)] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500');
+    const mediaQuery = window.matchMedia('(max-width: 500)');
+
+    setIsMobile(event.matches);
 
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
@@ -59,7 +61,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />

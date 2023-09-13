cc67765b3e2e50316f44274ae28737cbe86556c3
Emiko Esther S <graceupongraceemi@gmail.com>
2023-09-13 00:17:08 -0700

    Works.jsx modified

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { project } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadTest}>Work Experience.</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Works
+export default SectionWrapper(Works, '');

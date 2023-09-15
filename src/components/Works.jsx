e555c2a5516056042e409f1981565e558ca2f3a1
Emiko Esther S <graceupongraceemi@gmail.com>
2023-09-15 00:04:26 -0700

    ProjectCard component and projects map added to Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,6 +7,17 @@ import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link
+}) => {
+  return <motion.div>test</motion.div>;
+};
+
 const Works = () => {
   return (
     <>
@@ -26,6 +37,12 @@ const Works = () => {
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
       </div>
     </>
   );

af18312dfb166902080f99ad744f2de2a87eb8eb
Emiko Esther S <graceupongraceemi@gmail.com>
2023-09-26 08:09:10 -0700

    Stars.jsx updated with StarsCanvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -7,7 +7,14 @@ import CanvasLoader from '../Loader';
 const Earth = () => {
   const earth = useGLTF('./planet/scene.gltf');
 
-  return <primitive object={earth.scene}></primitive>;
+  return (
+    <primitive
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
+    ></primitive>
+  );
 };
 
 const EarthCanvas = () => {
@@ -16,7 +23,12 @@ const EarthCanvas = () => {
       shadows
       frameLoop='demand'
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
